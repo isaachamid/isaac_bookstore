aa62ec2d646eddca3d1de0e07b54665405c32686
hamidisaac <isaac.hamid@gmail.com>
2023-04-22 06:21:27 +0330

    add bootstrap and license

---
+++ isaac_bookstore.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * @link              https://github.com/isaachamid/isaac_bookstore
+ * @since             1.0.0
+ * @package           Isaac_bookstore
+ *
+ * @wordpress-plugin
+ * Plugin Name:       isaac_bookstore
+ * Plugin URI:        https://github.com/isaachamid
+ * Description:       Wordpress Plugin Book Store With Custom Taxonomy
+ * Version:           1.0.0
+ * Author:            Isaac
+ * Author URI:        https://github.com/isaachamid/isaac_bookstore
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       isaac_bookstore
+ * Domain Path:       /languages
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+require_once __DIR__ . '/vendor/autoload.php';
